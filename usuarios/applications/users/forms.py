3fb1c0992aa758747ba159e5dd6f92454fa17e78
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-11 17:54:01 -0500

    [feat] login validation

--- usuarios/applications/users/forms.py
+++ usuarios/applications/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User
+from django.contrib.auth import authenticate
 
 class UserRegisterForm(forms.ModelForm):
     
@@ -62,3 +63,13 @@ class LoginForm(forms.Form):
             } 
         )
     )
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        if not authenticate(username=username, password=password):
+            raise forms.ValidationError('Los datos de usuario no son correctos')
+
+        return self.cleaned_data
