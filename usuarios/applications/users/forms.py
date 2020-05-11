05953dd0528ecb10f6016e14dc9f55fec00da629
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-11 13:14:36 -0500

    [feat] register complete

--- usuarios/applications/users/forms.py
+++ usuarios/applications/users/forms.py
@@ -3,9 +3,38 @@ from .models import User
 
 class UserRegisterForm(forms.ModelForm):
     
+    password1 = forms.CharField(
+        label="Contraseña",
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            } 
+        )
+    )
+
+    password2 = forms.CharField(
+        label="Contraseña",
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir Contraseña'
+            } 
+        )
+    )
+
     class Meta:
         """Meta definition for UserRegisterForm."""
 
         model = User
-        fields = ('__all__')
+        fields = (
+            'username',
+            'email',
+            'nombres',
+            'apellidos',
+            'genero',
+        )
 
+    def clean_password2(self):
+        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+            self.add_error('password2', 'Las contraseñas no son iguales')
