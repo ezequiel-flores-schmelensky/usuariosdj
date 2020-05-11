fae84a00c7cd63b9edb06e4c38b315dce155e34d
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-11 13:24:35 -0500

    [feat] panel view

--- usuarios/usuarios/urls.py
+++ usuarios/usuarios/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('', include('applications.users.urls')),
+    re_path('', include('applications.home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
